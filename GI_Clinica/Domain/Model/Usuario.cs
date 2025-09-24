442b2e17c04202bbcf785269141e1205eac334ac
AlejoEchav <alejoechavarria123@gmail.com>
2025-09-24 15:01:13 -0500

    Commit

---
+++ GI_Clinica/Domain/Model/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GI_Clinica.Domain.ValueObjects;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace GI_Clinica.Domain.Model
+{
+    public class Usuario
+    {
+        public int IdUsuario;
+        public string NombreCompleto;
+        public DocumentoIdentidad Cedula;
+        public Email Correo;
+        public Telefono Telefono;
+        public DateTime FechaNacimiento;
+        public Direccion Direccion;
+        public string Rol;
+        public string UsuarioLogin;
+        public string Password;
+    }
+}
